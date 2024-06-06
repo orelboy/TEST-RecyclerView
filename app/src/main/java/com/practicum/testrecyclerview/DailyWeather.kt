c900d8fe8536ba28ed6e580558417c8f48d9234a
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2024-06-06 23:49:27 +0300

    Initial commit

---
+++ app/src/main/java/com/practicum/testrecyclerview/DailyWeather.kt
@@ -0,0 +1,6 @@
+package com.practicum.testrecyclerview
+
+data class DailyWeather(    val day: String,
+                            val clouds: Boolean,
+                            val temperature: Short
+    )
---
+++ app/src/main/java/com/practicum/testrecyclerview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.practicum.testrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val weatherAdapter = WeatherAdapter(
+            listOf(
+                DailyWeather("Пн", true, -23),
+                DailyWeather("Вт", true, -20),
+                DailyWeather("Ср", false, -15),
+                DailyWeather("Чт", true, -3),
+                DailyWeather("Пт", false, +6),
+                DailyWeather("Сб", false, +11),
+                DailyWeather("Вс", true, +20),
+            )
+        )
+
+        val rvWeather = findViewById<RecyclerView>(R.id.rvWeather)
+        rvWeather.adapter = weatherAdapter
+    }
+}
